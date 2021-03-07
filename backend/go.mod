b57dca8b73413ab0368344aaa88de40b8aba9619
Wim Fournier <github@fournier.nl>
2021-03-07 11:18:54 +0100

    more stubs

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,11 @@
 module github.com/hsmade/pi6atv-audio-control/backend
 
 go 1.16
+
+require (
+	github.com/hsmade/pi6atv-sensors/scraper v0.0.0-20201211143702-703abb6ca061
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.8.0
+	golang.org/x/exp v0.0.0-20210220032938-85be41e4509f
+	periph.io/x/periph v3.6.7+incompatible
+)
---
+++ backend/pkg/ina260/ina260.go
@@ -0,0 +1,81 @@
+package ina260
+
+import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+	"golang.org/x/exp/io/i2c"
+)
+
+type Ina260 struct {
+	device *i2c.Device
+	logger *logrus.Entry
+}
+
+func NewIna260(dev int) *Ina260 {
+	I := &Ina260{
+		logger: logrus.WithFields(logrus.Fields{
+			"address": fmt.Sprintf("%x", dev),
+			"package": "ina260",
+		}),
+	}
+
+	d, err := i2c.Open(&i2c.Devfs{Dev: "/dev/i2c-1"}, dev)
+	if err != nil {
+		I.logger.WithError(err).Fatal("failed to open i2c device")
+	}
+	I.device = d
+
+	if !I.isIna260() {
+		I.logger.Fatal("This is not an INA260")
+	}
+	return I
+}
+
+func (I *Ina260) Begin() error {
+	return I.reset()
+}
+
+func (I *Ina260) isIna260() bool {
+	buffer := make([]byte, 2)
+	err := I.device.ReadReg(0xFE, buffer)
+	return err == nil && len(buffer) == 2 && buffer[0] == 0x54 && buffer[1] == 0x49
+}
+
+func (I *Ina260) reset() error {
+	buffer := make([]byte, 2)
+	err := I.device.ReadReg(0x00, buffer)
+	if err != nil {
+		return err
+	}
+	buffer[1] |= 1 << 7
+	err = I.device.WriteReg(0x00, buffer)
+	return err
+}
+
+func (I *Ina260) readValue(address byte) (float64, error) {
+	buffer := make([]byte, 2)
+	err := I.device.ReadReg(address, buffer)
+	if err != nil {
+		return 0, err
+	}
+
+	value := float64(int(buffer[0])*256 + int(buffer[1]))
+	I.logger.Debugf("read from register %v: %v -> %f", address, buffer, value)
+	return value, nil
+}
+
+func (I *Ina260) ReadVoltage() (float64, error) {
+	v, err := I.readValue(0x02)
+	return v * 1.25 / 1000, err
+}
+
+func (I *Ina260) ReadCurrent() (float64, error) {
+	v, err := I.readValue(0x01)
+	return float64(int16(v)) * 1.25, err
+}
+
+func (I *Ina260) ReadPower() (float64, error) {
+	v, err := I.readValue(0x03)
+	return v * 10, err
+}
