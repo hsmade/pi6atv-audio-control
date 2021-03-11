53adb6a618722b4170693d059e5764c3cad5e0e5
Wim Fournier <github@fournier.nl>
2021-03-11 19:09:28 +0100

    simple cmd for testing

--- backend/cmd/backend.go
+++ backend/cmd/backend.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"github.com/hsmade/pi6atv-audio-control/backend/pkg/api"
+	"github.com/hsmade/pi6atv-audio-control/backend/pkg/config"
+	"log"
+	"net/http"
+	"periph.io/x/periph/host"
 
+)
+
+func main() {
+	host.Init()
+	router, err := api.NewRouter(config.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Fatal(http.ListenAndServe(":8001", router))
 }
